21c135d2332a6c8ec43d89f2d3b32551888226dd
ahmedukamel <ahmedukamel@duck.com>
2024-05-07 04:50:19 +0300

    edit path

--- src/main/java/org/ahmedukamel/arrafni/constant/PathConstants.java
+++ src/main/java/org/ahmedukamel/arrafni/constant/PathConstants.java
@@ -3,7 +3,8 @@ package org.ahmedukamel.arrafni.constant;
 import java.nio.file.Path;
 
 public interface PathConstants {
-    String prefix = "/home/ahmedukamel";
+    String prefix = "";
+    //    String prefix = "/home/ahmedukamel";
     Path BUSINESS_LOGO = Path.of(prefix + "/app/images/business/logo");
     Path BUSINESS_PICTURES = Path.of(prefix + "/app/images/business/pictures");
     Path USER_PROFILE_PICTURE = Path.of(prefix + "/app/images/profile/image");
