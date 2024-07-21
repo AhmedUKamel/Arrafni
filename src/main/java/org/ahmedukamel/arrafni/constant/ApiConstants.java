4e78bbd63deefde1c6c11a37a9071801b277e259
Ahmed K. <ahmedukamel@outlook.com>
2024-07-21 03:56:12 +0300

    update base url

--- src/main/java/org/ahmedukamel/arrafni/constant/ApiConstants.java
+++ src/main/java/org/ahmedukamel/arrafni/constant/ApiConstants.java
@@ -1,7 +1,7 @@
 package org.ahmedukamel.arrafni.constant;
 
 public interface ApiConstants {
-    String BASE_URL = "http://109.176.197.152:8088";
+    String BASE_URL = "https://api.arrafni.com";
     String MAIN_CATEGORY_LOGO_API = BASE_URL + "/api/v1/main-category/public/logo?logo=%s";
     String SUB_CATEGORY_LOGO_API = BASE_URL + "/api/v1/sub-category/public/logo?logo=%s";
     String ANNOUNCEMENT_POSTER_API = BASE_URL + "/api/v1/announcement/public/%s/poster";
